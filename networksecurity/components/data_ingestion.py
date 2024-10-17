e6974168166327463754667d58a49a1cd8003374
Yachit9 <mahajanyachit@gmail.com>
2024-10-17 21:58:15 +0530

    newcommit

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -24,17 +24,36 @@ class DataIngestion:
             raise NetworkSecurityException(e,sys)
     def export_collection_as_dataframe(self):
         try:
-            pass
+            database_name=self.data_ingestion_config.database_name
+            collection_name=self.data_ingestion_config.collection_name
+
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            collection=self.mongo_client[database_name][collection_name]
+
+            df=pd.DataFrame(list(collection.find()))
+
+            if "_id" in df.columns.to_list():
+                df=df.drop(columns=["_id"],axis=1)
+
+            df.replace({"na":np.nan},inplace=True)
+
+            return df
+
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def export_data_into_feature_store(self):
+    def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
-            pass
+            feature_store_file_path=self.data_ingestion_config.feature_store_file_path
+            #creating folder
+            dir_path=os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            dataframe_to_csv(feature_store_file_path,index=False,header=True)
+            return dataframe
         except Exception as e:
             raise NetworkSecurityException(e,sys)
     
-    def split_data_as_train_test(self):
+    def split_data_as_train_test(self,dataframe: pd.DataFrame):
         try:
             pass
         except Exception as e:
